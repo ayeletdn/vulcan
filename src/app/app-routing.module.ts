e6ed84081d78ff970039027a28b2bbc2ee2a6716
Ayelet Dahan <anativ@gmail.com>
2019-07-01 14:44:02 +0300

    Basic search component for specific CVE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CveDetailsComponent } from './cve-details/cve-details.component';
+import { CveSearchComponent } from './cve-search/cve-search.component';
 
 const routes: Routes = [
+  { path: '', component: CveSearchComponent},
   { path: 'cve/:id', component: CveDetailsComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CveDetailsComponent } from './cve-details/cve-details.component';
+import { CveSearchComponent } from './cve-search/cve-search.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CveDetailsComponent
+    CveDetailsComponent,
+    CveSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     NoopAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
